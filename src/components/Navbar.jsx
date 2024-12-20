cb0b3ed762d40cc34ff3d2396bbeac09abedc138
RyanFernando27 <138043611+RyanFernando27@users.noreply.github.com>
2024-12-21 00:45:25 +0530

    adding navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Button from "react-bootstrap/Button";
+import logo from "../assests/logo.jpg";
+
+import React from "react";
+
+function NavBar() {
+  return (
+    <Navbar className="bg-body-tertiary bg-red mb-5">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img
+            alt=""
+            src={logo}
+            width="50"
+            height="50"
+            className="d-inline-block align-top"
+          />{" "}
+          Real State
+        </Navbar.Brand>
+        <div className="mt-3">
+          <input type="text" placeholder="London"></input>
+          <div className="d-flex justify-content-center align-items-center mt-1 gap-2">
+            <Button variant="outline-success">For Sale</Button>
+            <Button variant="outline-success">For Rent</Button>
+          </div>
+        </div>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
