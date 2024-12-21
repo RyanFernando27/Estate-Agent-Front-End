802b56488fa5a0dd3513ef7e584a9e789bd91fda
RyanFernando27 <138043611+RyanFernando27@users.noreply.github.com>
2024-12-21 21:59:54 +0530

    adding items to favourite container functionality

--- src/components/Favourite.jsx
+++ src/components/Favourite.jsx
@@ -1,20 +1,59 @@
 import React from "react";
+import "../index.css";
 
-function Favourite() {
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import pic1 from "../assests/properties/prop1/prop1-1.jpg";
+
+function Favourite({ favourites, removeFromFavourite }) {
   return (
-    <div className="container ">
-      <div className="row justify-content-center">
-        <div className=" ">
-          <div className="border p-4 ">
-            <h1>Favourite</h1>
-            <p>
-              This container takes up half of the page width on medium and
-              larger screens.
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Container className="container-style">
+      <h1>Favourite</h1>
+      {/* Checking array has any items */}
+      {favourites.length > 0 ? (
+        // If favourites is not empty, it maps over the array and renders each favourite property inside a Row and Col layout.
+        <Row>
+          {favourites.map((fav) => (
+            <Col md={12} key={fav.id} className="mb-4">
+              <Card>
+                <Card.Body>
+                  <Row>
+                    <Col sm={4}>
+                      <Card.Img
+                        variant="top"
+                        src={pic1 || "placeholder-image-url.jpg"}
+                        alt={fav.type}
+                      />
+                    </Col>
+                    <Col sm={8}>
+                      <Card.Title>{fav.type}</Card.Title>
+                      <Card.Text>
+                        <strong>Tenure:</strong> {fav.tenure}
+                        <br />
+                        <strong>Bedrooms:</strong> {fav.bedrooms}
+                        <br />
+                        <strong>Location:</strong> {fav.location}
+                        <br />
+                        <strong>Price:</strong> ${fav.price.toLocaleString()}
+                      </Card.Text>
+                      {/* Remove button  for remove each item */}
+                      <Button
+                        variant="danger"
+                        onClick={() => removeFromFavourite(fav.id)}
+                      >
+                        Remove
+                      </Button>
+                    </Col>
+                  </Row>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      ) : (
+        // If there is no items in the favourite list this paragraph will display
+        <p>No items added to favourites yet.</p>
+      )}
+    </Container>
   );
 }
 
