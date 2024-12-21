d2426703af2a88c8166dd2973b5b1aac7ddf6732
RyanFernando27 <138043611+RyanFernando27@users.noreply.github.com>
2024-12-21 22:00:49 +0530

    addToFavouriteProp implementation

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,20 +1,16 @@
 import React from "react";
 import CardPage from "./CardPage";
 
-function Items() {
+function Items({ addToFavourite }) {
   return (
-    <>
-      <div className="container  ">
-        <div className="row justify-content-center ">
-          <div className="">
-            <div className="border p-4 row ">
-              <h1>Items</h1>
-              <CardPage />
-            </div>
-          </div>
+    <div className="container container-style">
+      <div className="row justify-content-center">
+        <div className="border p-4 row">
+          <h1>Items</h1>
+          <CardPage addToFavourite={addToFavourite} />
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Items from "./Items";
 import Row from "react-bootstrap/Row";
@@ -7,21 +7,38 @@ import Favourite from "./Favourite";
 import Col from "react-bootstrap/Col";
 
 function Main() {
+  const [favourites, setFavourites] = useState([]);
+
+  const addToFavourite = (item) => {
+    if (favourites.some((fav) => fav.id === item.id)) {
+      // Show alert if the item is already in the favourites list
+      alert("Item is already added");
+    } else {
+      setFavourites([...favourites, item]);
+    }
+  };
+
+  const removeFromFavourite = (id) => {
+    setFavourites(favourites.filter((fav) => fav.id !== id));
+  };
+
   return (
     <>
       <div className="container">
         <NavBar />
         <Row>
           <Col sm={8}>
-            <Items />
+            <Items addToFavourite={addToFavourite} />
           </Col>
           <Col sm={4}>
-            <Favourite />
+            <Favourite
+              favourites={favourites}
+              removeFromFavourite={removeFromFavourite}
+            />
           </Col>
         </Row>
       </div>
     </>
   );
 }
-
 export default Main;
