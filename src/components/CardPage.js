e0852f3fc0fe7871f72c926b3515c75e474a617a
RyanFernando27 <138043611+RyanFernando27@users.noreply.github.com>
2024-12-29 18:09:33 +0530

    add a property page through react dom

--- src/components/CardPage.js
+++ src/components/CardPage.js
@@ -2,48 +2,51 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import { createRoot } from "react-dom/client";
 
 function CardPage({ addToFavourite, properties, handleCardClick }) {
   return (
-    <Container className="my-4">
-      <Row>
-        {properties.map((eachCard) => (
-          <Col md={4} key={eachCard.id} className="mb-4">
-            <Card className="item-card">
-              <button
-                className="card-pic-btn"
-                onClick={() => handleCardClick(eachCard)}
-              >
-                <Card.Img
-                  variant="top"
-                  src={eachCard.picture}
-                  alt={eachCard.type}
-                />
-              </button>
-              <Card.Body>
-                <Card.Title>{eachCard.type}</Card.Title>
-                <Card.Text>
-                  <strong>Tenure:</strong> {eachCard.tenure}
-                  <br />
-                  <strong>Bedrooms:</strong> {eachCard.bedrooms}
-                  <br />
-                  <strong>Location:</strong> {eachCard.location}
-                  <br />
-                  <strong>Price:</strong> ${eachCard.price.toLocaleString()}
-                  <br />
-                </Card.Text>
-                <Button
-                  variant="primary"
-                  onClick={() => addToFavourite(eachCard)}
+    <>
+      <Container className="my-4">
+        <Row>
+          {properties.map((eachCard) => (
+            <Col md={4} key={eachCard.id} className="mb-4">
+              <Card className="item-card">
+                <button
+                  className="card-pic-btn"
+                  onClick={() => handleCardClick(eachCard)}
                 >
-                  Add to Favourite
-                </Button>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row>
-    </Container>
+                  <Card.Img
+                    variant="top"
+                    src={eachCard.picture}
+                    alt={eachCard.type}
+                  />
+                </button>
+                <Card.Body>
+                  <Card.Title>{eachCard.type}</Card.Title>
+                  <Card.Text>
+                    <strong>Tenure:</strong> {eachCard.tenure}
+                    <br />
+                    <strong>Bedrooms:</strong> {eachCard.bedrooms}
+                    <br />
+                    <strong>Location:</strong> {eachCard.location}
+                    <br />
+                    <strong>Price:</strong> ${eachCard.price.toLocaleString()}
+                    <br />
+                  </Card.Text>
+                  <Button
+                    variant="primary"
+                    onClick={() => addToFavourite(eachCard)}
+                  >
+                    Add to Favourite
+                  </Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </>
   );
 }
 
