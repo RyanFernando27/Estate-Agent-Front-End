1519799c85f2e72fe6776a9f6f0cdedf90c3abfa
RyanFernando27 <138043611+RyanFernando27@users.noreply.github.com>
2024-12-30 18:05:06 +0530

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import "./App.css";
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "./components/Main.js";
+import PropertyPage from "./components/PropertyPage.js";
+import Records from "./properties.json";
 
 function App() {
   return (
-    <>
-      <Main />
-    </>
+    <Router>
+      <Routes>
+        {/* Main Page */}
+        <Route path="/" element={<Main />} />
+
+        {/* Property Details Page */}
+        <Route
+          path="/product/:id"
+          element={<PropertyPage properties={Records.properties} />}
+        />
+      </Routes>
+    </Router>
   );
 }
 
