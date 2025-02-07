eb3b03449dc71381b29df98ca7fad8459957781c
RyanFernando27 <138043611+RyanFernando27@users.noreply.github.com>
2025-02-08 01:57:26 +0530

    add tilt function to cards

--- src/components/CardPage.js
+++ src/components/CardPage.js
@@ -3,6 +3,9 @@ import { Container, Row, Col } from "react-bootstrap";
 import { FaExternalLinkAlt } from "react-icons/fa";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import { Tilt } from 'react-tilt';
+import IconButton from "@mui/material/IconButton";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 
 function CardPage({
   addToFavourite,
@@ -10,50 +13,66 @@ function CardPage({
   handleShowModal,
   handleDragStart,
 }) {
+  const defaultOptions = {
+    reverse: false,
+    max: 15,
+    perspective: 1000,
+    scale: 1.05,
+    speed: 1000,
+    transition: true,
+    axis: null,
+    reset: true,
+    easing: "cubic-bezier(.03,.98,.52,.99)",
+  };
+
   return (
     <>
       <Container className="my-4">
         <Row>
           {properties.map((eachCard) => (
             <Col md={4} key={eachCard.id} className="mb-4">
-              <Card
-                className="item-card"
-                draggable // Enable dragging for the card
-                onDragStart={(e) => handleDragStart(e, eachCard)} // Use the passed drag start handler
-              >
-                <Card.Img
-                  variant="top"
-                  src={eachCard.picture}
-                  alt={eachCard.type}
-                />
-
-                <Card.Body>
-                  <Card.Title>{eachCard.type}</Card.Title>
-                  <Card.Text>
-                    <strong>Tenure:</strong> {eachCard.tenure}
-                    <br />
-                    <strong>Bedrooms:</strong> {eachCard.bedrooms}
-                    <br />
-                    <strong>Location:</strong> {eachCard.location}
-                    <br />
-                    <strong>Price:</strong> ${eachCard.price.toLocaleString()}
-                    <br />
-                  </Card.Text>
-                  <Button
-                    variant="link"
+              <Tilt options={defaultOptions}>
+                <Card
+                  className="item-card h-100"
+                  draggable // Enable dragging for the card
+                  onDragStart={(e) => handleDragStart(e, eachCard)} // Use the passed drag start handler
+                >
+                  <button
+                    className="card-pic-btn"
                     onClick={() => handleShowModal(eachCard.id)} // Trigger the modal
                   >
-                    View More <FaExternalLinkAlt />
-                  </Button>
+                    <Card.Img
+                      variant="top"
+                      src={eachCard.picture}
+                      alt={eachCard.type}
+                    />
+                  </button>
 
-                  <Button
-                    variant="success"
-                    onClick={() => addToFavourite(eachCard)}
-                  >
-                    Add to Favourite
-                  </Button>
-                </Card.Body>
-              </Card>
+                  <Card.Body>
+                    <Card.Title>{eachCard.type}</Card.Title>
+                    <Card.Text>
+                      <strong>Tenure:</strong> {eachCard.tenure}
+                      <br />
+                      <strong>Bedrooms:</strong> {eachCard.bedrooms}
+                      <br />
+                      <strong>Location:</strong> {eachCard.location}
+                      <br />
+                      <strong>Price:</strong> ${eachCard.price.toLocaleString()}
+                      <br />
+                    </Card.Text>
+                    <IconButton
+                      color="error"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        addToFavourite(eachCard);
+                      }}
+                      className="favorite-btn"
+                    >
+                      <FavoriteIcon />
+                    </IconButton>
+                  </Card.Body>
+                </Card>
+              </Tilt>
             </Col>
           ))}
         </Row>
